df77e315cfa0f9f1c8e9bfd29c7c472d02a569ec
akabel92 <ali.kabil92@gmail.com>
2016-06-23 15:44:10 +0200

    1.4 Update webService

--- SniperStocks/Controllers/webServiceController.swift
+++ SniperStocks/Controllers/webServiceController.swift
@@ -10,41 +10,21 @@ import UIKit
 import Alamofire
 class webServiceController: NSObject
 {
-    func test()
+    func getData(parameters:[String:String])
     {
-        let parameters =
-            [
-                "action": "login",
-                "user_name": "obada",
-                "user_password": "obada"
-        ]
-
-    Alamofire.request(.POST, "http://snniper.com/api/service.php", parameters: parameters)
-    .responseJSON { response in
-    print(response.request)  // original URL request
-    print(response.response) // URL response
-    print(response.data)     // server data
-    print(response.result)   // result of response serialization
-    
-    if let JSON = response.result.value {
-    print("JSON: \(JSON)")
-    }
-    }
+        Alamofire.request(.POST, "http://snniper.com/api/service.php", parameters: parameters)
+            .responseJSON
+            { response in
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+                print(response.data)     // server data
+                print(response.result)   // result of response serialization
+                
+                if let JSON = response.result.value
+                {
+                    print("JSON: \(JSON)")
+                }
+            }
         
-//        let headers = [
-//            "Content-Type": "application/x-www-form-urlencoded",
-//            "Accept": "application/json"
-//        ]
-//        
-//        Alamofire.request(.POST,
-//                          "http://snniper.com/api/service.php", parameters: parameters, encoding: . URLEncodedInURL, headers: headers).response
-//        { request, response, data, error in
-//            print(request)
-//            print(response)
-//            print(data)
-//            print(error)
-//            let dataString = NSString(data: data!, encoding:NSUTF8StringEncoding)
-//            print(dataString)
-//        }
-}
+    }
 }
